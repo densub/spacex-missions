b46a98ca6d8b013d4d878fb990595c9bed8b4fab
Denish Subedi <denishsubedi@Denishs-MacBook-Pro.local>
2022-03-30 23:58:26 -0500

    all done

--- src/App.js
+++ src/App.js
@@ -1,46 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
 import {
   ApolloClient,
   InMemoryCache,
-  ApolloProvider,
-  useQuery,
   gql
 } from "@apollo/client";
-import React, { Component } from 'react'
-import reactDom from 'react-dom';
+import React, { useState } from 'react'
+import moment from 'moment';
+import pageNotFound from './assets/notfound.png'
+import loadingIcon from './assets/revolvingrocket.gif'
+const QUERY = gql`
+query name {
+  launchesPast{
+    mission_name
+  links {
+    flickr_images
+    mission_patch_small
+  }
+  rocket {
+    rocket_name
+  }
+  launch_success
+  launch_date_utc
+}
+}
+`
 
-
-export default async function App() {
-  let launchPast
+export default function App() {
+  const [missions, getMissions] = useState([])
+  const [loading, setLoading] = useState(true)
+  let response
   const client = new ApolloClient({
     uri: 'https://api.spacex.land/graphql/',
     cache: new InMemoryCache()
   });
   
-  launchPast = await client
+  response = client
     .query({
-      query: gql`
-      query name {
-        launchesPast(limit: 10){
-          mission_name
-        links {
-          flickr_images
-          mission_patch_small
-        }
-        rocket {
-          rocket_name
-        }
-        launch_date_utc
-  }
-  }
-      `
+      query: QUERY
+    }).then((result) => {
+        console.log(result)
+        setLoading(false)
+        getMissions(result)
+    }).catch((error)=> {
+      console.log(error)
     })
-    console.log(launchPast.data.launchesPast)
-    
+
     return (
-      <h1>TEst</h1>
+     <>
+       {loading ? <div><img className='loading' src={loadingIcon}></img></div>:
+       <div className='MainDiv'>
+       <div className='DataView'> 
+       {missions?.data?.launchesPast.map((launchPost) => (
+        <table className='Column' key={launchPost.mission_name}>
+             <tr className='border-bottom details'>
+               <td width="20%">{launchPost.links.mission_patch_small != null ? <img className='missionPoster' src={launchPost.links.mission_patch_small}></img> : <img className='missionPoster' src={pageNotFound}></img>}</td>
+               <td className='description' width="70%">
+                <div className='missionName'>{launchPost.mission_name}</div> 
+                <div className='sub-details'>Rocket: {launchPost.rocket.rocket_name}</div> 
+                <div className='launchedAgo'>launched {moment(launchPost.launch_date_utc).fromNow()}</div>
+                </td>
+                <td  width="10%">{launchPost.launch_success ? <div className='success'>Success</div> : <div className='failure'>Failure</div>}</td> 
+             </tr>
+        </table>
+      ))}
+      </div>
+      </div>
+    }
+     </>
     )
+
 }
 
  
